182f41c236ae7e754dfcfe84def06e843316959a
otitoju <otitojuoluwapelumi@gmail.com>
2019-09-03 21:48:22 +0100

    remove unwanted files

--- server/model/user.js
+++ server/model/user.js
@@ -5,5 +5,5 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
-'mongodb+srv://yakubebank:kadzee222.@cluster0-opegc.mongodb.net/test?retryWrites=true&w=majority'
+
 module.exports = mongoose.model('user', userSchema)
